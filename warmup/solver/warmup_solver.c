068f4181a5251f31ce82f370671ab1b718ab2320
zulark <wihose2423@gmail.com>
2025-03-17 14:45:14 -0300

    Resolução do problema

--- warmup/solver/warmup_solver.c
+++ warmup/solver/warmup_solver.c
@@ -3,37 +3,50 @@
 const char OUTPUT_DIR[] = "output/";
 const char SOLUTION_FILE[] = "solution.txt";
 
-void solve_warmup(FILE* ptr_in_file, char* file_name, const char* warmup_instance) {
+void solve_warmup(FILE *ptr_in_file, char *file_name, const char *warmup_instance)
+{
 
     FILE *froutptr, *fwsolptr;
     char line[100];
     char out_file[100];
+    int W, N, P;
+    int golpes_justos_atingidos = 0;
 
     out_file[0] = '\0';
     strcat(out_file, warmup_instance);
     strcat(out_file, OUTPUT_DIR);
     strcat(out_file, file_name);
-    
+
     // Creating solution file
     fwsolptr = fopen(SOLUTION_FILE, "w");
-    if (fwsolptr == NULL) {
+    if (fwsolptr == NULL)
+    {
         printf("File '%s' can't be opened\n", SOLUTION_FILE);
         exit(1);
     }
 
-    /* *****************************************
-      Replace this code by your warmup solution
-      ****************************************** */
+    fscanf(ptr_in_file, "%d %d %d", &W, &N, &P);
+
+    for (int i = 0; i < P; i++)
+    {
+        int altura_do_soco;
+        fscanf(ptr_in_file, "%d", &altura_do_soco);
+        if (altura_do_soco >= W && altura_do_soco <= N)
+        {
+            golpes_justos_atingidos++;
+        }
+    }
 
     // Opening answer file
-    froutptr = fopen(out_file, "r");
-    if (froutptr == NULL) {
+    froutptr = fopen(out_file, "w");
+    if (froutptr == NULL)
+    {
         printf("File '%s' can't be opened\n", out_file);
         exit(1);
     }
-
     // Reading from the answer file and writing to the solution file
-    while (fgets(line, 100, froutptr)) {
+    while (fgets(line, 100, froutptr))
+    {
         fputs(line, fwsolptr);
     }
 
@@ -44,7 +57,8 @@ void solve_warmup(FILE* ptr_in_file, char* file_name, const char* warmup_instanc
     fclose(fwsolptr);
 }
 
-int check_warmup_solution(const char* file_name, const char* warmup_instance) {
+int check_warmup_solution(const char *file_name, const char *warmup_instance)
+{
 
     FILE *fanswer, *fsolution;
     char answer_line[100], solution_line[100], answer_file[100];
@@ -57,24 +71,28 @@ int check_warmup_solution(const char* file_name, const char* warmup_instance) {
 
     // Opening answer file
     fanswer = fopen(answer_file, "r");
-    if (fanswer == NULL) {
+    if (fanswer == NULL)
+    {
         printf("File '%s' can't be opened\n", answer_file);
         exit(1);
     }
 
     // Opening solution file
     fsolution = fopen(SOLUTION_FILE, "r");
-    if (fsolution == NULL) {
+    if (fsolution == NULL)
+    {
         printf("File '%s' can't be opened\n", SOLUTION_FILE);
         exit(1);
     }
 
     // Reading from the answer file and comparing with the solution file
-    while (fgets(answer_line, 100, fanswer)) {
+    while (fgets(answer_line, 100, fanswer))
+    {
 
         fgets(solution_line, 100, fsolution);
 
-        if (strcmp(answer_line, solution_line)) {
+        if (strcmp(answer_line, solution_line))
+        {
             is_correct = 0;
             break;
         }
