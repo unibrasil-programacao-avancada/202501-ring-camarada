0e9e768397c83797831f6d1b9e5ebe5478ec0a4e
zulark <wihose2423@gmail.com>
2025-03-31 14:20:07 -0300

    Alterado a solução do problema

--- warmup/solver/warmup_solver.c
+++ warmup/solver/warmup_solver.c
@@ -3,14 +3,10 @@
 const char OUTPUT_DIR[] = "output/";
 const char SOLUTION_FILE[] = "solution.txt";
 
-void solve_warmup(FILE *ptr_in_file, char *file_name, const char *warmup_instance)
-{
+void solve_warmup(FILE* ptr_in_file, char* file_name, const char* warmup_instance) {
 
-    FILE *froutptr, *fwsolptr;
-    char line[100];
+    FILE *fwsolptr;
     char out_file[100];
-    int W, N, P;
-    int golpes_justos_atingidos = 0;
 
     out_file[0] = '\0';
     strcat(out_file, warmup_instance);
@@ -19,46 +15,40 @@ void solve_warmup(FILE *ptr_in_file, char *file_name, const char *warmup_instanc
 
     // Creating solution file
     fwsolptr = fopen(SOLUTION_FILE, "w");
-    if (fwsolptr == NULL)
-    {
+    if (fwsolptr == NULL) {
         printf("File '%s' can't be opened\n", SOLUTION_FILE);
         exit(1);
     }
 
+    // Declaração de variáveis para armazenar as informações do problema
+    int W, N, P; // W (cintura), N (pescoço), P (número de socos)
+    int golpes_justos_atingidos = 0;  // Contador de golpes justos
+
+    // Lê a primeira linha do arquivo de entrada, que contém W, N e P
     fscanf(ptr_in_file, "%d %d %d", &W, &N, &P);
 
-    for (int i = 0; i < P; i++)
-    {
+    // Lê as alturas dos socos e conta quantos são "golpes justos"
+    // Um golpe é considerado justo se sua altura estiver entre W (mínimo) e N (máximo)
+    for (int i = 0; i < P; i++) {
         int altura_do_soco;
+
+        // Lê a altura do soco a partir do arquivo de entrada
         fscanf(ptr_in_file, "%d", &altura_do_soco);
-        if (altura_do_soco >= W && altura_do_soco <= N)
-        {
-            golpes_justos_atingidos++;
+
+        // Verifica se a altura do soco está dentro do intervalo de altura considerado justo
+        if (altura_do_soco >= W && altura_do_soco <= N) {
+            golpes_justos_atingidos++;  // Se for justo, incrementa o contador
         }
     }
 
-    // Opening answer file
-    froutptr = fopen(out_file, "w");
-    if (froutptr == NULL)
-    {
-        printf("File '%s' can't be opened\n", out_file);
-        exit(1);
-    }
-    // Reading from the answer file and writing to the solution file
-    while (fgets(line, 100, froutptr))
-    {
-        fputs(line, fwsolptr);
-    }
+    // Escreve o número de golpes justos atingidos no arquivo de solução
+    fprintf(fwsolptr, "%d\n", golpes_justos_atingidos);
 
-    fclose(froutptr);
-
-    /* *************************************** */
-
+    // Fecha o arquivo de solução após gravar o resultado
     fclose(fwsolptr);
 }
 
-int check_warmup_solution(const char *file_name, const char *warmup_instance)
-{
+int check_warmup_solution(const char* file_name, const char* warmup_instance) {
 
     FILE *fanswer, *fsolution;
     char answer_line[100], solution_line[100], answer_file[100];
@@ -71,28 +61,24 @@ int check_warmup_solution(const char *file_name, const char *warmup_instance)
 
     // Opening answer file
     fanswer = fopen(answer_file, "r");
-    if (fanswer == NULL)
-    {
+    if (fanswer == NULL) {
         printf("File '%s' can't be opened\n", answer_file);
         exit(1);
     }
 
     // Opening solution file
     fsolution = fopen(SOLUTION_FILE, "r");
-    if (fsolution == NULL)
-    {
+    if (fsolution == NULL) {
         printf("File '%s' can't be opened\n", SOLUTION_FILE);
         exit(1);
     }
 
     // Reading from the answer file and comparing with the solution file
-    while (fgets(answer_line, 100, fanswer))
-    {
+    while (fgets(answer_line, 100, fanswer)) {
 
         fgets(solution_line, 100, fsolution);
 
-        if (strcmp(answer_line, solution_line))
-        {
+        if (strcmp(answer_line, solution_line)) {
             is_correct = 0;
             break;
         }
